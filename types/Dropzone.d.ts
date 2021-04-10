a3b98796934588c0a3f3f54b01042b7530bb17aa
Jo Burgard <mail@joburgard.com>
2021-04-10 11:10:53 +0200

    Add new type information

--- types/Dropzone.d.ts
+++ types/Dropzone.d.ts
@@ -10,6 +10,13 @@ export interface DropzoneProps {
   direction?: "vertical" | "horizontal";
 
   dragHandle?: string;
+
+  copy?: boolean;
+
+  /**
+   * @default (item) => { copyId += 1; return Object.assign({}, item, { id: `copy_${copyId}` }); }
+   */
+  copyFunction?: (item: any) => any;
 }
 
 export default class Dropzone extends SvelteComponentTyped<
